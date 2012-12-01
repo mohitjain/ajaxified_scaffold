c57e2d2add24ddc2344b667fe32ae6b1d717a792
Mohit Jain <jain.mohit27@gmail.com>
2012-12-01 14:57:06 +0530

    default rails scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 AjaxifiedScaffold::Application.routes.draw do
+  resources :products
+
+  root :to => 'products#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
