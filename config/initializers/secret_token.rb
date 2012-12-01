bafba70913a5d623d53593307a12909f24c0bb34
Mohit Jain <jain.mohit27@gmail.com>
2012-12-01 14:55:28 +0530

    rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxifiedScaffold::Application.config.secret_token = '826ccae51b4002d8339d6a9f5f0d9af66bc77286b0838f0b868e38fe2fa3fc5aff80875542470d2364e18ca1148b050fbbfbe485966db6c3311f6cc23514ab0c'
