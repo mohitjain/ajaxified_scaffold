2e7f2aa25d6cf960ffea7d36d7950896599ca18c
Mohit Jain <jain.mohit27@gmail.com>
2013-01-15 23:30:18 +0530

    code added for validations display in ajax reqests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :price
+  
+  validates_presence_of :name
+  validates_numericality_of :price, :only_integer => true, :greater_than_or_equal_to => 0
 end
